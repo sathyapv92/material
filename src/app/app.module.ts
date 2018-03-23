d75730769d0e19760a43038473498f836cba7596
sathyapv92 <sathapv92@gmail.com>
2018-03-24 01:05:27 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { DialogComponent, DialogContentExampleDialog } from './dialog/dialog.component';
+import { MatDialogModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent,
+    DialogContentExampleDialog
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+   
+    MatDialogModule,
+    ReactiveFormsModule
+   
+    
+  
+   
+   
+  
+   
+   
+    
+   
+   
   ],
+  entryComponents: [DialogComponent, DialogContentExampleDialog],
   providers: [],
   bootstrap: [AppComponent]
 })
