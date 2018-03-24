8dffaf95768c9de0578ae59b326d71864113afe8
sathyapv92 <sathapv92@gmail.com>
2018-03-24 11:52:04 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,63 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { DialogComponent, DialogContentExampleDialog } from './dialog/dialog.component';
-import { MatDialogModule } from '@angular/material';
-import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule, MatFormField, MatFormFieldModule, MatInputModule, MatAutocompleteModule, MatButtonModule, MatCardModule, MatButtonToggleModule, MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatDividerModule, MatExpansionModule, MatGridListModule, MatIconModule, MatListModule, MatMenuModule, MatNativeDateModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, MatRadioModule, MatRippleModule, MatSelectModule, MatSidenavModule, MatSliderModule, MatSlideToggleModule, MatSnackBarModule, MatSortModule, MatStepperModule, MatTableModule, MatTabsModule, MatToolbarModule, MatTooltipModule } from '@angular/material';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { TestDialogComponent, DialogOverviewExampleDialog } from './test-dialog/test-dialog.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DialogComponent,
-    DialogContentExampleDialog
+    DialogContentExampleDialog,
+    TestDialogComponent,
+    DialogOverviewExampleDialog
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
    
     MatDialogModule,
-    ReactiveFormsModule
-   
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    FormsModule,
     
+    MatInputModule,
+
+   
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
   
    
    
@@ -32,7 +71,7 @@ import { ReactiveFormsModule } from '@angular/forms';
    
    
   ],
-  entryComponents: [DialogComponent, DialogContentExampleDialog],
+  entryComponents: [DialogComponent, DialogContentExampleDialog,TestDialogComponent,DialogOverviewExampleDialog],
   providers: [],
   bootstrap: [AppComponent]
 })
